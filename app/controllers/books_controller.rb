7f8f255f1afde824bd650b86852b3bb4d49d5645
Marcos A. Fernandes <contato@marcos-fernandes.com>
2019-11-13 11:08:21 -0300

    create and destroy books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,19 @@
 class BooksController < ApplicationController
   def index
-    render json: Book.all
+    render json: Book.order('title ASC')
   end
+
+  def create
+    Book.create(book_params)
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy
+  end
+
+  private
+    def book_params
+      params.permit(:title, :category)
+    end
+
 end
